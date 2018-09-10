245e5045f8ca865dadc99144418e41bdd679feee
rongfeiyue <839758456@qq.com>
2018-09-10 21:29:18 +0800

    logback

--- web/src/main/java/com/river/weibo/controller/WeiboController.java
+++ web/src/main/java/com/river/weibo/controller/WeiboController.java
@@ -2,7 +2,10 @@ package com.river.weibo.controller;
 
 import com.river.weibo.common.vo.PageInfoVO;
 import com.river.weibo.common.vo.ResponseVO;
+import com.river.weibo.common.vo.Weibo;
 import com.river.weibo.service.WeiboService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WeiboController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WeiboController.class);
 
     @Autowired
     private WeiboService weiboService;
@@ -24,6 +29,9 @@ public class WeiboController {
     @GetMapping("/getList")
     @ResponseBody
     public ResponseVO getList(PageInfoVO page) {
+        LOGGER.debug("debug msg");
+        LOGGER.info("info msg");
+        LOGGER.error("error msg");
         return new ResponseVO().buildSuccessResponse(weiboService.getList(page));
     }
 }
